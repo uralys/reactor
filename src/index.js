b3129aeaa550d4c91b7ba11c7391fa19e542fc24
chrisdugne <chris.dugne@uralys.com>
2021-03-13 21:37:14 +0100

    added GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import 'regenerator-runtime/runtime';
+// -----------------------------------------------------------------------------
+
 import React from 'react';
 import {render} from 'react-dom';
+import {createGlobalStyle} from 'styled-components';
+
+// -----------------------------------------------------------------------------
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+// -----------------------------------------------------------------------------
 
 const createYourApp = (options = {}) => {
   if (!options.container) {
@@ -10,6 +23,7 @@ const createYourApp = (options = {}) => {
   const container = document.getElementById(options.container);
   render(
     <div>
+      <GlobalStyle />
       <b>🚀 skeleton-react</b>
     </div>,
     container
