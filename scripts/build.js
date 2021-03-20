c8185432fac4c890c4330b7e3866158578e7b9f8
chrisdugne <chris.dugne@uralys.com>
2021-03-20 21:30:52 +0100

    simplified build

--- scripts/build.js
+++ scripts/build.js
@@ -1,70 +1,35 @@
 const chalk = require('chalk');
 const esbuild = require('esbuild');
 const svgrPlugin = require('esbuild-plugin-svgr');
-const brotli = require('./compress');
 
-const DIST = 'dist';
-const bundleName = `app`;
+console.log('☢️  warming esbuild...');
 
-const banner = `/**
- * BUNDLED with esbuild
- * (c) 2020 Evan Wallace
- * https://github.com/evanw/esbuild
- * @license MIT
- */
-`;
+const outfile = 'public/app.min.js';
 
-const resultMessage = results => {
-  console.log(`${chalk.green(' ✔ Success')}`);
+esbuild
+  .build({
+    entryPoints: ['src/index.js'],
+    minify: true,
+    bundle: true,
+    metafile: true,
+    outfile,
+    loader: {
+      '.js': 'jsx'
+    },
+    plugins: [svgrPlugin()],
+    define: {
+      'process.env.NODE_ENV': '"production"',
+      global: 'globalThis'
+    }
+  })
+  .then(result => {
+    console.log(`${chalk.green(' ✔ Success')}`);
+    const nbBytes = result.metafile.outputs[`${outfile}`].bytes;
 
-  results.forEach(({output, size}) => {
     console.log(
-      `   ${chalk.cyan('→')} ${chalk.hex('#D07CFF').bold(`${output}`)} ${
-        size ? chalk.yellow(size) : ''
-      }`
+      ` ${chalk.cyan('→')} ${chalk
+        .hex('#C07CFF')
+        .bold(`${outfile}`)} (${nbBytes})`
     );
-  });
-
-  console.log(' ');
-};
-
-const buildApp = ({format, minify, compress}) => {
-  const MODE = minify ? 'min' : 'debug';
-  const outfile = `${DIST}/${MODE}/${bundleName}.${MODE}.js`;
-
-  esbuild
-    .build({
-      banner: {js: banner},
-      format,
-      minify,
-      entryPoints: ['src/index.js'],
-      bundle: true,
-      sourcemap: true,
-      metafile: true,
-      outfile,
-      loader: {'.js': 'jsx'},
-      plugins: [svgrPlugin()],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'globalThis'
-      }
-    })
-    .then(result => {
-      const metafile = result.metafile;
-      if (compress) {
-        brotli(metafile).then(results => {
-          resultMessage(results);
-        });
-      } else {
-        resultMessage([{output: outfile}]);
-      }
-    })
-    .catch(() => process.exit(1));
-};
-
-const formats = [
-  {format: 'cjs', minify: true, compress: true},
-  {format: 'cjs', minify: false, compress: false}
-];
-
-formats.forEach(buildApp);
+  })
+  .catch(() => process.exit(1));
--- scripts/prepare-public-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo '👨🏽‍🚀 Preparing public files';
-cp -r public/ dist/public/
-cp -r dist/min/* dist/public/.
-
-echo '👨🏽‍🚀 ✅ done!';
