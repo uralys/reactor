cbb83ac0a00a8545fe049d0d4dfd2a5d45dcde20
chrisdugne <chris.dugne@uralys.com>
2021-03-30 17:53:39 +0200

    fixed metafile

--- src/lib/write-metafile.js
+++ src/lib/write-metafile.js
@@ -4,7 +4,7 @@ const output = 'meta.json';
 module.exports = metafile => {
   console.log(`☢️  exporting metafile in ${output}`);
   try {
-    fs.writeFileSync(output, JSON.stringify());
+    fs.writeFileSync(output, JSON.stringify(metafile));
   } catch (e) {
     console.log(e);
   }
